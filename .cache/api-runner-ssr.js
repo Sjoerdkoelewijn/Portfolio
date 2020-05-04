21103df8e8b9ac55c49d568f20034783f3509f0f
Sjoerd Koelewijn <s.koelewijn@gmail.com>
2020-05-04 14:10:02 +0200

    moved analytics to the header

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -11,6 +11,12 @@ var plugins = [{
       plugin: require('D:/development/Portfolio/Gatsby/node_modules/gatsby-plugin-prefetch-google-fonts/gatsby-ssr'),
       options: {"plugins":[],"fonts":[{"family":"Heebo","variants":["300","400","500","800","900"]}]},
     },{
+      plugin: require('D:/development/Portfolio/Gatsby/node_modules/gatsby-plugin-google-tagmanager/gatsby-ssr'),
+      options: {"plugins":[],"id":"GTM-5RWBB6W","includeInDevelopment":false,"defaultDataLayer":{"type":"object","value":{"platform":"gatsby"}}},
+    },{
+      plugin: require('D:/development/Portfolio/Gatsby/node_modules/gatsby-plugin-google-analytics/gatsby-ssr'),
+      options: {"plugins":[],"trackingId":"UA-4523109-3","head":false,"anonymize":true,"respectDNT":true,"exclude":["/preview/**"],"pageTransitionDelay":0,"optimizeId":"OPT-MG7R8L9","defer":false,"sampleRate":100,"siteSpeedSampleRate":100,"cookieDomain":"sjoerdkoelewijn.com"},
+    },{
       plugin: require('D:/development/Portfolio/Gatsby/node_modules/gatsby-plugin-offline/gatsby-ssr'),
       options: {"plugins":[],"precachePages":["/","/contact/","/portfolio/*"]},
     }]
