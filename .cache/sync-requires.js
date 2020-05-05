3f9ab8a22c1207bce96870e0ac4a013878e959a6
Sjoerd Koelewijn <s.koelewijn@gmail.com>
2020-05-05 16:23:34 +0200

    minor css fixed for mobile

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,11 +5,11 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("D:\\development\\Portfolio\\Gatsby\\.cache\\caches\\gatsby-plugin-offline\\app-shell.js"))),
   "component---src-templates-portfolio-js": hot(preferDefault(require("D:\\development\\Portfolio\\Gatsby\\src\\templates\\portfolio.js"))),
   "component---src-templates-service-js": hot(preferDefault(require("D:\\development\\Portfolio\\Gatsby\\src\\templates\\service.js"))),
   "component---src-templates-post-js": hot(preferDefault(require("D:\\development\\Portfolio\\Gatsby\\src\\templates\\post.js"))),
   "component---src-templates-page-js": hot(preferDefault(require("D:\\development\\Portfolio\\Gatsby\\src\\templates\\page.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("D:\\development\\Portfolio\\Gatsby\\.cache\\dev-404-page.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("D:\\development\\Portfolio\\Gatsby\\src\\pages\\index.js")))
 }
 
